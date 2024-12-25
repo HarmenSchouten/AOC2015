64e5c09e7e50ab7f712faabe18a4b3b2c976e89a
Harmen Schouten <harmenschouten@outlook.com>
2024-12-25 15:35:01 +0100

    Day 04

---
+++ Day04/Part2.ts
@@ -0,0 +1,13 @@
+import "../utils/index.ts"
+import * as crypto from 'node:crypto'
+const input = await Deno.readTextFile("./day04/input.txt")
+
+let i = 0;
+while (true) {
+    const result = crypto.createHash('md5').update(`${input}${i}`).digest('hex')
+    if (result.startsWith('000000')) {
+        console.log(i)
+        break;
+    }
+    i++
+}
