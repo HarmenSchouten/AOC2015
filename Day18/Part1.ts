4e964a3aaf86b87d9c68a0b61e6bb00b5e00ded4
Harmen <harmenschouten@outlook.com>
2025-10-21 19:37:12 +0200

    Day 18

---
+++ Day18/Part1.ts
@@ -0,0 +1,27 @@
+import { Grid2D } from "../utils/grid2d.ts";
+import "../utils/index.ts"
+const input = await Deno.readTextFile("./day18/input.txt")
+
+const grid = new Grid2D<string>(input)
+
+for (let i = 0; i < 100; i++) {
+    const onCells = grid.filter(x => {
+        if (x.value !== "#") return false;
+        const neighbourCnt = x.getAllNeighbours(false, c => c.value === "#").length
+        return (neighbourCnt === 3 || neighbourCnt === 2)
+    }).concat(
+        grid.filter(x => {
+        if (x.value !== '.') return false;
+        const neighbourCnt = x.getAllNeighbours(false, c => c.value === "#").length
+        return neighbourCnt === 3
+    }))
+
+    for (let x = 0; x < grid.width; x++) {
+        for (let y = 0; y < grid.height; y++) {
+            const isOn = onCells.find(c => c.x === x && c.y === y)
+            grid.set(x, y, isOn ? '#' : '.')
+        }
+    }
+}
+
+console.log(grid.filter(x => x.value === '#').length)
